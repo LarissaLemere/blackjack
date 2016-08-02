32fa2bc95adeb8982cc850f20bec223dadb8dfb9
Larissa Lemere <Larissa4Rose@gmail.com>
2016-08-02 02:57:22 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,55 +6,31 @@ public class Card {
 	// can modify them.
 	private String suit;
 	private int rank;
-
-
 	// Constructor receives the suit and rank as parameters.
-	public Card (String s, int r)
-	{
+	public Card (String s, int r){
 		this.suit = s;
 		this.rank = r; 
 	}
 
-
 	// Returns the rank of the current card.
-	public int getRank()
-	{
+	public int getRank(){
 		return this.rank; 
 	}
-
-
 	// Returns the string that represents the card (ex. Ace of Spades)
-	public String printCard()
-	{
+	public String printCard()	{
 		String s = "";
-
 		if (this.rank == 1)
-		{
 			s += "Ace";
-		}
 		else if (this.rank == 11)
-		{
 			s += "Jack";
-		}
 		else if (this.rank == 12)
-		{
 			s += "Queen";
-		}
 		else if (this.rank == 13)
-		{
 			s += "King";
-		}
 		else
-		{
 			s += this.rank;
-		}
 
 		s += " of " + this.suit;
 		return s;
-
 	}
-
-
-
-
 }
